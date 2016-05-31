aa04fc6db3953f0ff302d69a1c9ab92b124e20fd
akucherevskiy <a.kucherevskiy@gmail.com>
2016-05-31 19:49:52 +0300

    product page

--- web/index.php
+++ web/index.php
@@ -111,9 +111,9 @@ $app->get('/product/{id}', function ($id) use ($app){
 	->bind('product');
 
 $app->get('/concept', function () use ($app){
-	$sql = "SELECT * FROM products left JOIN img on img.user_id = products.id where img.alias='shop' and products.is_concept = 1 order by products.id";
+	$sql = "SELECT *, products.id as ids, products.name as sname FROM products left JOIN img on img.user_id = products.id where img.alias='shop' and products.is_concept =1 and img.type = 1 order by products.id";
 	$dataimg = $app['db']->fetchAll($sql);
-var_dump($dataimg); die;
+
 	return $app['twig']->render('concept.twig', ['data'=>$dataimg]);
 })
 	->bind('concept');
