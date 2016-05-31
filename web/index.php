58740481f5f9a57129405733fd7356ca0700d299
akucherevskiy <a.kucherevskiy@gmail.com>
2016-05-31 18:46:16 +0300

    added product page & upload several images

--- web/index.php
+++ web/index.php
@@ -100,7 +100,7 @@ $app->get('/shop', function () use ($app){
 $app->get('/product/{id}', function ($id) use ($app){
 	$sql = "SELECT * FROM products left JOIN img on img.user_id = products.id where img.alias='shop' and products.id=". $id;
 	$dataimg = $app['db']->fetchAll($sql);
-
+var_dump($dataimg); die;
 	return $app['twig']->render('product.twig', ['data'=>$dataimg]);
 })
 	->bind('product');
