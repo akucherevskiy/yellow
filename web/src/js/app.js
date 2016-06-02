420e478cf44c8c5be01bbecbf035b984ed360510
akucherevskiy <a.kucherevskiy@gmail.com>
2016-06-03 00:11:11 +0300

    all prod butt

--- web/src/js/app.js
+++
@@ -1,6 +0,0 @@
-import aboutGallery from './about-gallery'
-import shop from './shop'
-import shopItemGallery from './shop-item-gallery'
-import basket from './add-to-basket'
-import googMap from './google-maps'
-
